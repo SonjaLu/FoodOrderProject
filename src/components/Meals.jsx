f85ef8c6ace76d9525e70e869a99f5bd57cb631a
SonjaLu <s_lueg@yahoo.com>
2024-04-24 21:58:15 +0200

    try catch added

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -1,19 +1,29 @@
-import 
+import { useState, useEffect } from 'react';
 
 export default function Meals() {
-async function fetchMeals() {
-    try {
-     const response = await fetch('http://localhost:3000/meals');
-    } catch (error) {
-        if (!response.ok) {
-            throw new Error('Failed to fetch meals');
-        }
+const [loadedMeals, setLoadedMeals] = useState([]);
 
-        const meals = await response.json();
-}
+useEffect(() => {
+    async function fetchMeals() {
+        try {
+         const response = await fetch('http://localhost:3000/meals');
+       
+            if (!response.ok) {
+                throw new Error('Failed to fetch meals');
+            }
+    
+            const meals = await response.json();
+            setLoadedMeals(meals);
+        } catch (error) {
+            console.log(error.message);
+        }
+    }
+    fetchMeals();
+}, []);
+
 
     
   return (
-    <ul id="meals"></ul>
+    <ul id="meals">{loadedMeals.map(meal => <li key={meal.id}>{meal.name}</li>)}</ul>
   )  
-}}
+}
